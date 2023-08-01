dcb8393d6e102bacd26fcabc8cd9d3d30ff279dd
Gabriel Musskopf <gabrielgmusskopf@gmail.com>
2023-07-31 23:52:00 -0300

    adicionar polybar

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Add this script to your wm startup file.
+# sudo ln -s ~/.config/polybar/launch.sh /etc/init.d/polybar.sh
+
+DIR="$HOME/.config/polybar/"
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch the bar
+polybar -q mybar -c "$DIR"/config.ini &
